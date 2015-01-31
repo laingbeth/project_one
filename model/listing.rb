2663dca4d199114e9d427fc411f1f116893b280e
beth laing <laingbeth@gmail.com>
2015-01-31 13:15:01 -0500

    added glyphs

--- model/listing.rb
+++ model/listing.rb
@@ -13,10 +13,11 @@ class Listing
                      comments: 'Best deal for the price and parking is free!',
                      best_dish: 'The Pambazo',
                      location: '4166 Buford Hwy Atlanta GA 30345',
-                     website:  'http://www.plazafiesta.net/',
+                     website:  'www.plazafiesta.net',
                      description: "Legend tells of a sandwich made from torta
                bread dipped in ranchero sauce, filled with chorizo sausage,
-               potatoes, lettuce, cheese & sour cream."
+               potatoes, lettuce, cheese & sour cream.",
+                      image: 'plaza_fiesta.jpeg'
                ),
       Restaurant.new(id: 20, recommender: 'Dave',
                      category: 'Home Cookin',
@@ -25,7 +26,7 @@ class Listing
                      comments: 'When someone cooks!',
                      best_dish: 'Scrambled eggs and toast',
                      location: '1234 Buford Hwy Atlanta GA 30345',
-                     website:  'http://www.mykitchen.net/',
+                     website:  'www.mykitchen.net',
                      description: 'We may not cook often but when we do it is really great.'
               )
     ]
